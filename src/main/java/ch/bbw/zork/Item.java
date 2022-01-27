4e2ac5a58929e983383590637c9d1dd086040f37
Tabea Bolliger <bolligertabea@gmail.com>
2022-01-27 09:31:07 +0100

    Initial commit

---
+++ src/main/java/ch/bbw/zork/Item.java
@@ -0,0 +1,33 @@
+package ch.bbw.zork;
+
+public class Item {
+	private String name;
+	private String description;
+	private int weight;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+
+}
---
+++ src/main/java/ch/bbw/zork/Z.java
@@ -0,0 +1,13 @@
+package ch.bbw.zork;
+
+public class Z {
+
+	public static void main(String[] args) {
+		Game zorkgame = new Game();
+		zorkgame.play();
+	}
+
+}
+
+
+
